b7d1d1988e6f39618e5aaf747f9d33622a66a1e7
vb1980 <vb1980@sina.com>
2025-08-21 22:49:25 +0800

    enable form login

--- trunk/user/httpd/httpd.c
+++ trunk/user/httpd/httpd.c
@@ -538,7 +538,7 @@ http_login_check(const uaddr *ip_now)
 		return 2;
 	}
 
-	return 0;
+	return 3;
 }
 
 static int
@@ -636,19 +636,6 @@ send_error( int status, const char *title, const char *extra_header, const char 
 	fprintf( conn_fp, "%s\n", text );
 	fprintf( conn_fp, "</BODY></HTML>\n" );
 	fflush( conn_fp );
-}
-
-static void
-send_authenticate( FILE *conn_fp )
-{
-	char header[128], *realm;
-
-	realm = nvram_safe_get("computer_name");
-	if (strlen(realm) < 1)
-		realm = nvram_safe_get("productid");
-
-	snprintf(header, sizeof(header), "WWW-Authenticate: Basic realm=\"%s\"", realm);
-	send_error( 401, "Unauthorized", header, "Authorization required.", conn_fp );
 }
 
 static int
@@ -873,7 +860,6 @@ handle_request(FILE *conn_fp, const conn_item_t *item)
 		if ( strcmp( cur, "\n" ) == 0 || strcmp( cur, "\r\n" ) == 0 ) {
 			break;
 		}
-		
 		if (strncasecmp(cur, "Accept-Language:", 16) == 0) {
 			if (!http_has_lang)
 				http_has_lang = set_preferred_lang(cur + 16);
@@ -949,14 +935,13 @@ handle_request(FILE *conn_fp, const conn_item_t *item)
 	usockaddr_to_uaddr(&item->usa, &conn_ip);
 
 	login_state = http_login_check(&conn_ip);
-	
+
 	if (login_state == 0) {
 		if (strstr(file, ".htm") != NULL || strstr(file, ".asp") != NULL) {
 			file = "Nologin.asp";
 			query = NULL;
 		}
 	}
-	
 
 	/* special case for reset browser credentials */
 	if (strcmp(file, "logout") == 0) {
@@ -981,16 +966,23 @@ handle_request(FILE *conn_fp, const conn_item_t *item)
 	do_logout = (strcmp(file, "Logout.asp") == 0) ? 1 : 0;
 
 	if (handler->need_auth && login_state > 1 && !do_logout) {
-		if (!auth_check(authorization)) {
-			http_logout(&conn_ip);
-			if (method_id == HTTP_METHOD_POST)
-				eat_post_data(conn_fp, clen);
-			send_authenticate(conn_fp);
-			return;
+		/*
+			没登录需要验证
+		*/
+		if (login_state == 2){
+			if(auth_check(authorization)){
+				http_login(&conn_ip);
+				send_headers( 200, "OK", NULL, NULL, NULL, conn_fp );
+				return;
+			}else {
+				http_logout(&conn_ip);
+				if (method_id == HTTP_METHOD_POST)
+					eat_post_data(conn_fp, clen);
+				file = "Login.asp";
+				query = NULL;
+			}
+
 		}
-		
-		if (login_state == 2)
-			http_login(&conn_ip);
 	}
 
 	if (method_id == HTTP_METHOD_POST) {
